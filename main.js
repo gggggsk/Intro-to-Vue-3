23e458b5dc3ed62cb7cab59ddf046a22b0a4b8ba
Your Name <you@example.com>
2021-03-31 09:13:03 +0700

    update

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color:'green'},
-                { id: 2234, color:'blue'}
-            ]
-        }
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      inStock: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2234, color: "blue" },
+      ],
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
     }
-})
+  }
+});
